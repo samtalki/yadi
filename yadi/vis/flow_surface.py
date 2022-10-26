7c06429a6f2797fa45251955cb91ccaa864c6530
samtalki <saimouer@gmail.com>
2022-10-26 12:48:11 -0400

    first commit; viz module and flow_surface

---
+++ yadi/vis/flow_surface.py
@@ -0,0 +1,21 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import yadi.dss.sensitivity as sens
+
+class FlowSurface:
+
+    def __init__(self,path):
+        pass
+
+    def make_flow_surface():
+        pass
+
+    def plot_line_surface():
+        pass
+
+        
+    def get_loads():
+        pass
+
+    def get_gens():
+        pass
