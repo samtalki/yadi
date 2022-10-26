1a415282774e6b4a3cbdaa9fc924fc5942c57cfb
samtalki <saimouer@gmail.com>
2022-10-26 14:49:41 -0400

    add line flows to yadi.dss.model

--- yadi/dss/model.py
+++ yadi/dss/model.py
@@ -8,7 +8,7 @@ October 2021
 import numpy as np
 import pandas as pd
 import opendssdirect as dss
-
+import math
 
 ELEMENT_CLASSES = {
     'Load': dss.Loads,
@@ -22,11 +22,13 @@ LINE_CLASSES = ['Line', 'Xfmr']
 
 class DSS_Data:
     """
+    @author: Samuel Talkington and Jorge Fernandez
     Data class for OpenDSS network models.
     """
 
     def __init__(self, redirects, verbose=True):
         """
+        @author: Samuel Talkington
         Initializes an OpenDSS network model. 
         
         Params:s
@@ -67,22 +69,80 @@ class DSS_Data:
             print(f'DSS Running file: {filename}')
         self.run_command(f'redirect "{filename}"')
 
-    
-    ##Get methods
-    @staticmethod
-    def get_all_elements(element='Load'):
+
+    ##
+    # Setter methods
+    ##
+
+    def set_loads(self, loadP, loadQ, loadNames):
+        """
+        @author: Jorge Fernandez
+        """
+
+        # rename nodes to explicit load names
+        loadP = loadP.loc[loadNames.index].rename(index=loadNames)
+        loadQ = loadQ.loc[loadNames.index].rename(index=loadNames)
+                
+        # modify loads
+        self.__modify_loads(loadP, loadQ)
+
+
+    def __modify_loads(self, instDemandP, instDemandQ):
+        """
+        @author: Jorge Fernandez
+        Method to modify loads from a DSS file according to dispatch
+        """
+        
+        elems = self.dss.circuit_all_element_names()
+        for i, elem in enumerate(elems):
+            self.dss.circuit_set_active_element(elem)
+            if "Load" in elem:   
+                # extract load name
+                loadName = elem.split(".")[1]
+                # write load name
+                self.dss.loads_write_name(loadName)
+                # read kvar
+                kvar = instDemandQ[loadName]
+                # save kw
+                kw = instDemandP[loadName]
+                if math.isnan(kw):
+                    breakpoint()
+                self.__modify_load(kw, kvar,  loadName)
+
+    def __modify_load(self, kw, kvar, load):
+        self.dss.text(f"edit load.{load} "
+                      f"kw={kw} "
+                      f"kvar={kvar}")
+
+    ##
+    # Getter methods
+    ##
+
+    def get_all_elements(self,element='Load'):
         if element in ELEMENT_CLASSES:
             cls = ELEMENT_CLASSES[element]
-            df = dss.utils.to_dataframe(cls)
+            df = self.dss.utils.to_dataframe(cls)
         else:
-            df = dss.utils.class_to_dataframe(element, transform_string=lambda x: pd.to_numeric(x, errors='ignore'))
+            df = self.dss.utils.class_to_dataframe(element, transform_string=lambda x: pd.to_numeric(x, errors='ignore'))
             # df = dss.utils.class_to_dataframe(element)
         return df
 
+    def get_line_flows(self, lname):
+        """
+        @author: Jorge Fernandez
+        Given a list of line names lname, return 4 tuple (Pjk,Qjk,pjk,qjk) 
+        """
+        # get line flows
+        lines, lines_KW_power, lines_KVAR_power = self.__get_line_powers()
+        
+        # organize line flows for node-based analysis
+        Pjk, Qjk, pjk, qjk = self.__read_line_flows(lines, lines_KW_power, lines_KVAR_power, lname)
+        return Pjk, Qjk, pjk, qjk
 
-    
+
     def get_node_voltages(self):
         """
+        @author: Samuel Talkington
         Get static dictionary of all node voltages in the system at a single timestep
         """
         voltages_dict = dict()
@@ -108,6 +168,7 @@ class DSS_Data:
     
     def get_node_currents(self):
         """
+        @author: Samuel Talkington
         Get static dictionary of all node currents in the system at a single timestep
         """
         currents_dict = dict()
@@ -129,21 +190,28 @@ class DSS_Data:
 
     def get_node_complex_powers(self):
         """
+        @author: Samuel Talkington
         Get static dictionary of all node complex powers in the system at a single timestep
         """
-        powers_dict = dict()
+
+        self.powers_dict = dict()
+
         if(self.currents_dict == None or self.voltages_dict == None):
             raise Exception('No internal currents dict or voltages dict found')
+        
         for node in self.dss.Circuit.YNodeOrder():
-            powers_dict[node] = self.voltages_dict[node]*np.conjugate(self.currents_dict[node]) #S=VI*
-        self.powers_dict = powers_dict
-        return powers_dict
+            self.powers_dict[node] = self.voltages_dict[node]*np.conjugate(self.currents_dict[node]) #S=VI*
+        
+        return self.powers_dict
 
 
     def get_node_ybus(self, init):
+        """
+        @author: Samuel Talkington and Jorge Fernandez
+        """
         
         if init == True:
-            self.__initialization()
+            self.__initialize_dss()
 
         # initialize OpenDSS solver
         self.dss.run_command("solve")
@@ -179,9 +247,77 @@ class DSS_Data:
         
         return Y_net, volts
 
+    
+    def __read_line_flows(self, lines, lines_KW_power, lines_KVAR_power, lname):
+        """
+        @author: Jorge Fernandez
+        """
+        # active flows
+        Pjk = np.zeros([len(lname)])  # containing flows Pkm
+        Pkj = np.zeros([len(lname)])  # containing flows Pmk
+        # reactive flows
+        Qjk = np.zeros([len(lname)])  # containing flows Pkm
+        Qkj = np.zeros([len(lname)])  # containing flows Pmk
+        # aggregated per line
+        pjk = np.zeros([len(lines)])  # containing flows Pkm
+        qjk = np.zeros([len(lines)])  # containing flows Pkm
+        cont = 0
+        for l, line in enumerate(lines):
 
-    def __initialization(self):
-        """Initializies basic DSS parameters"""
+            p = np.asarray(lines_KW_power[line])
+            q = np.asarray(lines_KVAR_power[line])
+
+            Pjk[cont: cont + int(len(p)/2)] = p[:int(len(p)/2)]
+            Pkj[cont: cont + int(len(p)/2)] = p[int(len(p)/2):]
+
+            Qjk[cont: cont + int(len(p)/2)] = q[:int(len(p)/2)]
+            Qkj[cont: cont + int(len(p)/2)] = q[int(len(p)/2):]
+
+            # total line flow
+            pjk[l] = sum(p[:int(len(p)/2)])
+            qjk[l] = sum(q[:int(len(p)/2)])
+
+            cont += int(len(p)/2)
+
+        return Pjk, Qjk, pjk, qjk
+
+    #TODO: Change PyDSS interface calls to OpenDSSDirect.
+    def __get_line_powers(self):
+        """
+        @author: Jorge Fernandez and Samuel Talkington
+        """
+        # prelocate 
+        lines = list()
+        lines_KW_dict = dict()
+        lines_KVAR_dict = dict()
+        # dss elements
+        elements = self.dss.circuit_all_element_names()
+        for i, elem in enumerate(elements):
+            self.dss.circuit_set_active_element(elem)
+            if "Line" in elem:
+                lines.append(elem)
+                lines_KW_dict[elem] = self.dss.cktelement_powers()[0::2]
+                lines_KVAR_dict[elem] = self.dss.cktelement_powers()[1::2]
+
+            elif "Transformer" in elem:
+                lines.append(elem)
+                if elem == 'Transformer.xfm1':
+                    lines_KW_dict[elem] = self.dss.cktelement_powers()[0::2]
+                    lines_KVAR_dict[elem] = self.dss.cktelement_powers()[1::2]
+                    del lines_KW_dict[elem][3]
+                    del lines_KW_dict[elem][-1]
+                    del lines_KVAR_dict[elem][3]
+                    del lines_KVAR_dict[elem][-1]
+                else: 
+                    lines_KW_dict[elem] = [i for i in self.dss.cktelement_powers()[0::2] if i != 0]
+                    lines_KVAR_dict[elem] = [i for i in self.dss.cktelement_powers()[1::2] if i != 0]
+        return lines, lines_KW_dict, lines_KVAR_dict
+
+    def __initialize_dss(self):
+        """
+        @author: Samuel Talkington
+        Initializies basic DSS parameters
+        """
         # set maxiterations number
         self.dss.run_command("Set Maxiterations=600")
         # disable the default regulator
