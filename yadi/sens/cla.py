f586fc738d10090dab197e2c219ec9632f84b190
samtalki <saimouer@gmail.com>
2023-07-04 11:25:09 -0400

    cla class first commit

---
+++ yadi/sens/cla.py
@@ -0,0 +1,15 @@
+## Conservative linear approximations
+
+
+class DSS_CLA:
+
+    def __init__(self) -> None:
+        pass
+
+    def calc_cla(self):
+        pass
+
+    def calc_cla_sens(self):
+        pass
+
+    
